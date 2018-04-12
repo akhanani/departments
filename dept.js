763a029db919781cc22bf223786bb0312fa9a894
akhanani <sosoakha@gmail.com>
2018-04-12 12:21:16 +0200

    printed all the totals

---
+++ dept.js
@@ -0,0 +1,28 @@
+console.log('hello')
+
+var toiletriesDept = {name : 'toiletries',total : 5007}
+var groceriesDept= {name:' groceries',total: 11089 }
+var pharmacyDept = {name :'pharmacy',total : 7321 }
+var bakeryDept = {name :'bakery', total :10073}
+var butcheryDept ={name : 'bitchery',total :9073}
+
+var depts =[];
+depts.push(toiletriesDept);
+depts.push(groceriesDept);
+depts.push(pharmacyDept);
+depts.push(bakeryDept);
+depts.push( butcheryDept);
+
+for(var i=0;i<depts.length;i++){
+  console.log(depts[i].total);
+}
+/*
+console.log(toiletriesDept);
+console.log (groceriesDept);
+console.log( pharmacyDept);
+console.log (bakeryDept);
+console.log(butcheryDept);
+*/
+function grandTotal(depts){
+  console.log(depts);
+}
