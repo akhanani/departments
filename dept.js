077b0af16d66d33b217c576d799f49124f5a9e74
akhanani <sosoakha@gmail.com>
2018-04-13 15:43:23 +0200

    doing dept-exercise

--- dept.js
+++ dept.js
@@ -4,7 +4,7 @@ var toiletriesDept = {name : 'toiletries',total : 5007}
 var groceriesDept= {name:' groceries',total: 11089 }
 var pharmacyDept = {name :'pharmacy',total : 7321 }
 var bakeryDept = {name :'bakery', total :10073}
-var butcheryDept ={name : 'bitchery',total :9073}
+var butcheryDept ={name : 'bitchery',total :9053}
 
 var depts =[];
 depts.push(toiletriesDept);
@@ -15,6 +15,7 @@ depts.push( butcheryDept);
 
 for(var i=0;i<depts.length;i++){
   console.log(depts[i].total);
+  console.log(depts[i].name);
 }
 /*
 console.log(toiletriesDept);
@@ -26,3 +27,10 @@ console.log(butcheryDept);
 function grandTotal(depts){
   console.log(depts);
 }
+
+grandTotal(depts)
+var total = 0;
+for (var i=0;i<depts.length;i++){
+total += (depts[i].total);
+}
+console.log(total);
